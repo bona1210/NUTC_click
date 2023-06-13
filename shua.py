bc669a0a2e61991f48719d9504adc526837edeb7
bona_PC <jimmy249872041@gmail.com>
2023-06-13 20:13:22 +0800

    ver.23.06.13.2013

--- shua.py
+++ shua.py
@@ -5,12 +5,12 @@ import time
 import sys
 
 # 設定網址
-urls = "https://elearn.nutc.edu.tw/forum/m_node_chain.php?cid=10071642&bid=1000202245&nid=000000001"
+urls = "輸入步驟一"
  
 
 print("\n開始訪問網頁...")
 # 設定次數
-brushNum = 4
+brushNum = 150
 
 for i in range(brushNum):
     # 設定timeout (單位：秒)
@@ -20,7 +20,7 @@ for i in range(brushNum):
     
     headers = {
  'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
- 'Cookie':'PHPSESSID=4f0a6ul4tq24ujlo2kj2r3phs5; sIdx=; _ga=GA1.3.1664925445.1663918416; school_hash=f1f431772863feb541000143e736476d; wm_lang=Big5; idx=399adbdd08c09b6502032f545fea8272'}
+ 'Cookie':'輸入步驟二'}
     req = urllib.request.Request(urls, headers=headers)
     urllib.request.urlopen(req).read()    
     # 計算次數
